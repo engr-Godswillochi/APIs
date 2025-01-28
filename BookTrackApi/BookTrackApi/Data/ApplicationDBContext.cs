3ab246d5836ca5ae3d6c59ee0f5f69718fa39fe9
engr-Godswillochi <ochigodswill@gmail.com>
2025-01-28 17:20:07 +0100

    connected database

--- BookTrackApi/BookTrackApi/Data/ApplicationDBContext.cs
+++ BookTrackApi/BookTrackApi/Data/ApplicationDBContext.cs
@@ -1,6 +1,18 @@
-﻿namespace BookTrackApi.Data
+﻿using BookTrackApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookTrackApi.Data
 {
-    public class ApplicationDBContext
+    public class ApplicationDBContext : DbContext
     {
+        
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(options: dbContextOptions)
+        {
+
+        }
+        public DbSet<Book> books { get; set; }
+        public DbSet<Collection> collections { get; set; }
+        public DbSet<ReadingProgress> readingProgress { get; set; }
+        public DbSet<Review> reviews { get; set; }
     }
 }
