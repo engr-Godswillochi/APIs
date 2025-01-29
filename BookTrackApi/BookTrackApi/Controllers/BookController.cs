7955d49bc0e1b812e5e0053b5960b7db9461c724
engr-Godswillochi <ochigodswill@gmail.com>
2025-01-29 03:26:53 +0100

    fixed some errors

--- BookTrackApi/BookTrackApi/Controllers/BookController.cs
+++ BookTrackApi/BookTrackApi/Controllers/BookController.cs
@@ -1,25 +1,33 @@
-﻿//using BookTrackApi.Interface;
-//using Microsoft.AspNetCore.Mvc;
-//using BookTrackApi.Mappers;
-//namespace BookTrackApi.Controllers
-//{
-//    [ApiController]
-//    [Route("api/book")]
-//    public class BookController : ControllerBase
-//    {
-//        private readonly IBookRepository _bookRepository;
-//        public BookController(IBookRepository bookRepository)
-//        {
-//            _bookRepository = bookRepository;
-//        }
-
-//        [HttpGet]
-//        public async Task<IActionResult> GetAll()
-//        {
-//            var books = await _bookRepository.GetAllAsync();
-//            var BookDto = books.Select(s => s.BookDto()).ToList();
-
-//            return Ok(BookDto);
-//        }
-//    }
-//}
+﻿using BookTrackApi.Interface;
+using Microsoft.AspNetCore.Mvc;
+using BookTrackApi.Mappers;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using BookTrackApi.Data;
+using System.Data.Entity;
+namespace BookTrackApi.Controllers
+{
+    [ApiController]
+    [Route("api/book")]
+    public class BookController : ControllerBase
+    {
+        //private readonly IBookRepository _bookRepository;
+        //public BookController(IBookRepository bookRepository)
+        //{
+        //    _bookRepository = bookRepository;
+        //}
+        private readonly ApplicationDBContext _context;
+        public BookController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        [Authorize]
+        [HttpGet("my-books")]
+        public async Task<IActionResult> GetMyBooks()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var books = await _context.Books.ToListAsync();
+            return Ok(books);
+        }
+    }
+}
